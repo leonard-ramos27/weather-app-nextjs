2cb4892e2dfb35c652e68b84b0a3047b98046db3
Leonard Ramos <ramos27leonard@gmail.com>
2025-09-20 15:24:52 +0800

    Initial commit for Weather app

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div>Weather Now</div>
+      <h1>How's the sky looking today?</h1>
+    </div>
+  );
+}
