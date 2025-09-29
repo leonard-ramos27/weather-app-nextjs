f1dc8c0b9ddae4db729c53565e6efcc4a3ba651b
Leonard Ramos <ramos27leonard@gmail.com>
2025-09-29 17:22:18 +0800

    Created test data for daily forecast

---
+++ data/daily-forecast.tsx
@@ -0,0 +1,24 @@
+
+const daily_forecast_raw = {
+    "latitude":14.625,
+    "longitude":121.0,
+    "generationtime_ms":5.944728851318359,
+    "utc_offset_seconds":28800,
+    "timezone":"Asia/Manila",
+    "timezone_abbreviation":"GMT+8",
+    "elevation":9.0,
+    "daily_units":{
+        "time":"iso8601",
+        "weather_code":"wmo code",
+        "temperature_2m_max":"°C",
+        "temperature_2m_min":"°C"
+    },
+    "daily":{
+        "time":["2025-08-05","2025-08-06","2025-08-07","2025-08-08","2025-08-09","2025-08-10","2025-08-11"],
+        "weather_code":[80,51,0,2,95,71,45],
+        "temperature_2m_max":[20,21,24,25,21,25,24],
+        "temperature_2m_min":[14,15,14,13,15,16,15]
+    }
+}
+
+export default daily_forecast_raw
