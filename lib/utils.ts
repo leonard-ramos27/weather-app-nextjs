1770c58f9512ea79227d6b6c3c0adc0d208860eb
Leonard Ramos <ramos27leonard@gmail.com>
2025-09-29 17:23:23 +0800

    Created util function to format daily forecast data

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
+import { DailyForecast } from "@/types/forecast"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function transformDailyData(daily: {
+  time: string[],
+  weather_code: number[],
+  temperature_2m_max: number[],
+  temperature_2m_min: number[]
+}): DailyForecast[] {
+  return daily.time.map((time, i) => ({
+    time,
+    weatherCode: daily.weather_code[i],
+    temperatureMax: daily.temperature_2m_max[i],
+    temperatureMin: daily.temperature_2m_min[i],
+  }));
 }
