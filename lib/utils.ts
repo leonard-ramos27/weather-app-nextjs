343c4e065b21b72717f5e27d69c3b0df577e3cca
Leonard Ramos <ramos27leonard@gmail.com>
2025-10-03 00:03:09 +0800

    Created function to transform hourly forecast data

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { DailyForecast } from "@/types/forecast"
+import { DailyForecast, HourlyForecast } from "@/types/forecast"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -18,4 +18,33 @@ export function transformDailyData(daily: {
     temperatureMax: daily.temperature_2m_max[i],
     temperatureMin: daily.temperature_2m_min[i],
   }));
+}
+
+export function getDatesFromHourlyData(hourly : {
+  time: string[],
+  temperature_2m: number[],
+  weather_code: number[]
+}): string[] {
+  const dates = hourly.time.map((t) => t.split("T")[0])
+  return [...new Set(dates)]
+}
+
+export function transformHourlyData(hourly : {
+  time: string[],
+  temperature_2m: number[],
+  weather_code: number[]
+}, date: string
+): HourlyForecast[] {
+  const data: HourlyForecast[] = []
+  hourly.time.map((timestamp, index) => {
+    const [day, hour] = timestamp.split("T")
+    if(day === date) {
+      data.push({
+        time: timestamp,
+        weatherCode: hourly.weather_code[index],
+        temperature: hourly.temperature_2m[index]
+      })
+    }
+  })
+  return data
 }
