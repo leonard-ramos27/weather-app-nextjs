01014a2565a651bd238815178b85c51f0bd396da
Leonard Ramos <ramos27leonard@gmail.com>
2025-10-05 23:29:54 +0800

    Created function to fetch weather data from api

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,8 @@
 'use client';
 
+import { WeatherData } from "@/types/forecast";
 import { LocationData, SearchSuggestions } from "@/types/search-suggestions";
+import { PrecipType, TempType, WindSpeedType } from "@/types/units";
 import useSWR from "swr";
 
 const fetcher = (...args: Parameters<typeof fetch>) => fetch(...args).then(res => res.json())
@@ -29,4 +31,28 @@ export async function getCoordinates(query: string): Promise<LocationData | null
         })
     }
     return locationMatch
+}
+
+interface GetWeatherDataProps {
+    latitude: number,
+    longitude: number,
+    temp_unit: TempType,
+    wind_speed_unit: WindSpeedType,
+    precip_unit: PrecipType
+}
+
+export function getWeatherData({latitude, longitude, temp_unit, wind_speed_unit, precip_unit}: GetWeatherDataProps) {
+    const base_url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation,weather_code`;
+
+    const units_params = `${wind_speed_unit === "mph" ? '&wind_speed_unit=mph' : ''}${temp_unit === 'Fahrenheit' ? '&temperature_unit=fahrenheit' : ''}${precip_unit === 'Inches' ? '&precipitation_unit=inch' : ''}`;
+    
+    const api_url = base_url + units_params;
+
+    const { data, isLoading, error } = useSWR(api_url, fetcher)
+
+    return {
+        weather_data: data as WeatherData,
+        isLoading,
+        error
+    }
 }
